5d5b0cc9af3b87a368840bab9dfffc40ba6b28c2
shubhamlahoti01 <lahotishubham03@gmail.com>
2023-07-15 09:42:55 +0530

    footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from "react";
-import "./styles/footer.scss";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
+import "./styles/footer.css"; // Import your CSS file for styling
 
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <footer className="footer-container">
+      <div className="social-icons">
+        <a href="https://www.facebook.com">
+          <FaFacebook className="icon" />
+        </a>
+        <a href="https://www.twitter.com">
+          <FaTwitter className="icon" />
+        </a>
+        <a href="https://www.instagram.com">
+          <FaInstagram className="icon" />
+        </a>
+      </div>
+      <div className="copywrite">© 2023 HoMeDeCoR. All rights reserved.</div>
+    </footer>
+  );
 };
 
 export default Footer;
